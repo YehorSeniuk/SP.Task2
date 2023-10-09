863d3328e324c8239860afc8eda1a76b19bda3f1
YehorSeniuk <seniuk.yehor@gmail.com>
2023-10-09 11:20:40 +0300

    a few strings

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,12 @@
 #include "calculator.h"
 
+//Хто тримає цей район?
+//Пес Патрон, пес Патрон
+//Хто крутіший за iPhone?
+//Пес Патрон, пес Патрон
+//Seniuk
+//Yehor
+
 int Calculator::Add (double a, double b)
 {
     return a + b + 0.5;
